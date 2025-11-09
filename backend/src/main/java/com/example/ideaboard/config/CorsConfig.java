1e486d1847afe34a3c512e7990f63f8482f0db55
dkempego <dkempego@asu.edu>
2025-11-09 09:57:38 -0700

    review idea created
    review idea created

--- backend/src/main/java/com/example/ideaboard/config/CorsConfig.java
+++ backend/src/main/java/com/example/ideaboard/config/CorsConfig.java
@@ -1,13 +1,10 @@
 package com.example.ideaboard.config;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
